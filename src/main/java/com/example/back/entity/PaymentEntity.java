b1b40b0db032d834d7f279fc671b5be45af90499
orderabbit <dur369667@naver.com>
2024-06-12 22:50:51 +0900

    save payment error

--- src/main/java/com/example/back/entity/PaymentEntity.java
+++ src/main/java/com/example/back/entity/PaymentEntity.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -25,4 +27,5 @@ public class PaymentEntity {
     private String customerPhone;
     private String amount;
     private String paymentKey;
+    private List<Long> productIds;
 }
